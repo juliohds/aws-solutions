0451c2ee88f07b54be5dd08db8325b43b4f33b96
Julio Henrique <tecjuliohenrique@gmail.com>
2018-06-25 16:03:34 -0300

    necessary

--- app/config.php
+++ app/config.php
@@ -3,11 +3,11 @@
 return [
     's3' => [
         'credentials' => array(
-            'key' => 'AKIAJBWWSXOPGVH6V5MA',
-            'secret'  => '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',
+            'key' => 'a',
+            'secret'  => 'a',
         ),
         'region' => 'us-east-1',
-        'bucket' => 'julio-br',
-        'version' => 'latest'
+        'bucket' => 'a-br',
+        'version' => 'a'
     ]
 ];
